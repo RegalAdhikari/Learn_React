545dfb43d93c6f46bee2428a9870c2dc7d8ee7aa
Regal Adhikari <65963700+RegalAdhikari@users.noreply.github.com>
2026-02-07 14:19:59 +0545

    quotes-wacky

---
+++ random-facts/src/heading.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Heading = () => {
+    return (
+        <div className="flex flex-col">
+            <p className="justify-center flex align-middle m-10 text-4xl">
+                Random Facts
+            </p>
+            <p className="justify-center flex align-middle text-2xl">
+                Learn a LOT while you sit on the POT
+            </p>
+        </div>
+    );
+};
+
+export default Heading;
